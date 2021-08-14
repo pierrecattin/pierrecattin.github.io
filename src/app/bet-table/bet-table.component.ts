d0f7d81e4c3ef59158d334b7b00cd64114a77db8
Pierre Cattin <cattin.pierre@gmail.com>
2021-08-14 23:10:48 +0200

    logging

--- src/app/bet-table/bet-table.component.ts
+++ src/app/bet-table/bet-table.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChange } from '@angular/core';
 import { Bet } from '../bet';
-import { BoardComponent } from '../board/board.component';
 
 export interface betTable {
   cellKey: string;
@@ -13,27 +12,26 @@ export interface betTable {
   templateUrl: './bet-table.component.html',
   styleUrls: ['./bet-table.component.scss']
 })
-export class BetTableComponent implements OnInit  {
-  @Input() bets!: Bet[];
+export class BetTableComponent implements OnChanges  {
+  @Input('bets') bets!:Bet[];
+
   displayedColumns: string[] = ['cellKey', 'amount'];
   dataSource: betTable[]=[];
   
-  constructor() { 
-    this.dataSource  = [
-      {cellKey: "Red", amount: 10},
-      {cellKey: "Black", amount: 20},
-      {cellKey: "1", amount: 20}
-    ];
+
+  ngOnChanges(changes: {[propName: string]: SimpleChange}) {
+    console.log("change: size of bets= " +this.bets.length);
+    this.refreshBetsTable();
   }
 
-  ngOnInit(){
+
+  refreshBetsTable(){
+    this.bets=[];
     for(let bet of this.bets){
-      this.addBet(bet);
+      this.addBetToTable(bet);
     }
   }
-
-  addBet(bet:Bet):void{
-    // Check https://angular.io/guide/component-interaction
+  addBetToTable(bet:Bet):void{
     let newEntry: betTable[]=[{cellKey:bet.cellKey, amount:bet.amount}];
     this.dataSource.push(newEntry[0]);
   }

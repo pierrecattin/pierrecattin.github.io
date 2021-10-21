7f63b73b126689a10e876c25a3320a1458a1a800
Pierre Cattin <cattin.pierre@gmail.com>
2021-10-21 23:15:54 +0200

    mainnet addresses

--- contract/DeCa.sol
+++ contract/DeCa.sol
@@ -74,12 +74,12 @@ contract DeCa is ERC20, VRFConsumerBase {
     constructor()
         ERC20("DeCa", "DCA")
         VRFConsumerBase(
-            0x8C7382F9D8f56b33781fE506E897a4F1e2d17255,
-            0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+            0x3d2341ADb2D31f1c5530cDC622016af293177AE0,
+            0xb0897686c545045aFc77CF20eC7A532E3120E0F1
         )
     {
         // VRF Coordinator; LINK Token
-        s_keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4; // TODO: s_keyHash =keyHash;
+        s_keyHash = 0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da; // TODO: s_keyHash =keyHash;
         s_fee = 10**(18 - 4); // TODO: s_fee = fee;
 
         _mint(msg.sender, 1000000 * 10**decimals()); //TODO: initialSupply
